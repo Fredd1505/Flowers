96c26bde7b55446e30e41b0e9fe69313ee1d80a2
Fredd <159171201+Fredd1505@users.noreply.github.com>
2025-09-24 17:12:16 -0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Small gift for someone huge').split('')
+    const titles = ('¡Small gift for someone huge!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
